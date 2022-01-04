e4187eaf8417683a40e31815a30b46e87aaa405c
Louis Lefevre <louislefev@gmail.com>
2022-01-04 03:06:59 +0000

    Add TextView marquee initial delay

--- app/src/main/java/com/example/musicplayer/adapters/SwipeSongAdapter.kt
+++ app/src/main/java/com/example/musicplayer/adapters/SwipeSongAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.musicplayer.adapters
 
+import android.widget.TextView
+import androidx.core.view.postDelayed
 import com.example.musicplayer.R
 import com.example.musicplayer.data.entities.Song
+import com.example.musicplayer.misc.Constants.MARQUEE_INITIAL_DELAY
 import kotlinx.android.synthetic.main.swipe_item.view.*
 
 class SwipeSongAdapter(private val onSongClicked: (Song) -> Unit) : BaseSongAdapter(R.layout.swipe_item) {
@@ -9,11 +12,23 @@ class SwipeSongAdapter(private val onSongClicked: (Song) -> Unit) : BaseSongAdap
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         val song = getItem(position)
         holder.itemView.apply {
-            tvSongTitle.text = song.title
-            tvSongArtist.text = song.artist
+            tvSongTitle.apply {
+                text = song.title
+                setupTextViewMarquee(this)
+            }
+            tvSongArtist.apply {
+                text = song.artist
+                setupTextViewMarquee(this)
+            }
             setOnClickListener {
                 onSongClicked(song)
             }
+        }
+    }
+
+    private fun setupTextViewMarquee(textView: TextView) {
+        textView.postDelayed(MARQUEE_INITIAL_DELAY) {
+            textView.isSelected = true
         }
     }
 }
--- app/src/main/java/com/example/musicplayer/misc/Constants.kt
+++ app/src/main/java/com/example/musicplayer/misc/Constants.kt
@@ -10,6 +10,8 @@ object Constants {
 
     const val UPDATE_PLAYER_POSITION_INTERVAL = 100L
 
+    const val MARQUEE_INITIAL_DELAY = 3000L
+
     const val NOTIFICATION_ID = 1
     const val NOTIFICATION_CHANNEL_ID = "music"
 
